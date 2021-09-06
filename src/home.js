51e42dea517341940f050d51da8b5ea1258a5ca1
Diegogl91 <garciadiego03@gmail.com>
2021-09-06 01:07:29 +0000

    content working

--- src/home.js
+++ src/home.js
@@ -36,18 +36,14 @@ const Home = () => {
                                 < div className="card mb-3" key={index} >
                                     <div className="row g-0">
                                         <div className="col-md-4">
-                                            <img src="..." className="img-fluid rounded-start" alt="..." />
+                                            <img src={image} className="img-fluid rounded-start" alt="..." />
                                         </div>
                                         <div className="col-md-8">
                                             <div className="card-body">
-                                                <h5 className="card-title">Card title</h5>
-                                                <p className="card-text">
-                                                    This is a wider card with supporting text below as a natural lead-in
-                                                    to additional content. This content is a little bit longer.
-                                                </p>
-                                                <p className="card-text">
-                                                    <small className="text-muted">Last updated 3 mins ago</small>
-                                                </p>
+                                                <h5 className="card-title">{name}</h5>
+                                                <p className="card-text">{status} </p>
+                                                <p className="card-text">{species}</p>
+                                                <p className="card-text">{gender}</p>
                                             </div>
                                         </div>
                                     </div>
